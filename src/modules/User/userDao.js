443983bd5448f10d791ce75fd9fb405f539c2948
naodevtech <naimtahouri@gmail.com>
2022-04-18 00:52:40 +0200

    :label: (config): add user relations :label:

--- src/modules/User/userDao.js
+++ src/modules/User/userDao.js
@@ -8,7 +8,14 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      this.hasMany(models.UserContract, {
+        foreignKey: 'userId',
+        onDelete: 'CASCADE'
+      });
+      this.hasMany(models.Subscribtion, {
+        foreignKey: 'userId',
+        onDelete: 'CASCADE'
+      });
     }
   }
   User.init(
