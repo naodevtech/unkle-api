021833b495f484b9866fb0afdc50b74106a5b01c
naodevtech <naimtahouri@gmail.com>
2022-04-18 00:56:12 +0200

    :label: (config): add option relations :label:

--- src/modules/ContractOption/contractoptionDao.js
+++ src/modules/ContractOption/contractoptionDao.js
@@ -8,7 +8,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      this.hasMany(models.ContractOption, {
+        foreignKey: 'optionId',
+        onDelete: 'CASCADE'
+      });
     }
   }
   ContractOption.init(
